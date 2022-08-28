222898de7f35b014f8bbd140b580871c8e8e702b
New User <laptop@News-MacBook-Pro.local>
2022-08-29 00:34:53 +0100

    get all users worked on

--- backend/src/controller/user.ts
+++ backend/src/controller/user.ts
@@ -10,7 +10,8 @@ export async function signupUser (req: Request, res: Response, next: NextFunctio
 
     const id = uuidv4()
     try{ 
-        const validationResult = registerSchema.validate(req.body,options)
+        const validationResult = registerSchema.validate(req.body, options)
+        
         const duplicatEmail = await UserInstance.findOne({where:{email:req.body.email}})
         if(duplicatEmail){
          return res.status(409).json({
@@ -52,16 +53,45 @@ export async function signupUser (req: Request, res: Response, next: NextFunctio
        })
     }
  
-
-    
 };
 
 /* GET users listing. */
-export async function loginUser (req: Request, res: Response, next: NextFunction) {
-    res.json({
-        msg: "login user route"
-    });
-};
+export async function loginUser(req: Request, res: Response, next: NextFunction) {
+    const id = uuidv4()
+    try {
+        const validationResult = loginSchema.validate(req.body, options)
+        if (validationResult.error) {
+            return res.status(400).json({
+                Error: validationResult.error.details[0].message
+            })
+        }
+        const User = await UserInstance.findOne({ where: { email: req.body.email } }) as unknown as { [key: string]: string }
+        
+        const { id } = User;
+        const token = generateToken({ id })
+        const validUser = await bcrypt.compare(req.body.password, User.password);
+
+        if (!validUser) {
+            res.status(401).json({
+                message: "Password do not match"
+            })
+        }
+        if (validUser) {
+            res.json({
+                msg: "login user route"
+            })
+        }
+        }catch (err) {
+            console.log(err);
+            res.status(500).json({
+                msg: 'failed to login',
+                route: '/login'
+            })
+        }
+        
+}
+
+
 
 /* GET users listing. */
 export async function logoutUser (req: Request, res: Response, next: NextFunction) {
@@ -71,17 +101,39 @@ export async function logoutUser (req: Request, res: Response, next: NextFunctio
 };
 
 /* GET ALL USERS users listing. */
-export async function getUsers (req: Request, res: Response, next: NextFunction) {
-    res.json({
-        msg: "get all users route"
-    });
-};
+export async function getUsers(req: Request, res: Response, next: NextFunction) {
+    try {
+        const limit = req.query?.limit as number | undefined;
+        const offset = req.query?.offset as number | undefined;
+        const record = await UserInstance.findAll({where: {}})
+        res.status(200).json({
+            msg: "You have succesfully fetch all app",
+            record 
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: 'failed to read',
+            route: "/read"
+        });
+    }
+}
 
 /* GET SINGLE users listing. */
-export async function getSingleUser (req: Request, res: Response, next: NextFunction) {
-    res.json({
-        msg: "get single user route"
-    });
+export async function getSingleUser(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { id } = req.params;
+        const record = await UserInstance.findOne({ where: { id } });
+        // return record 
+        return res.status(200).json({
+            msg: "get single user route",
+            record
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: "failed to read single user",
+            route: "/read/:id"
+        })
+    }
 };
 
 /* UPDATE users listing. */
@@ -92,10 +144,26 @@ export async function updateUser (req: Request, res: Response, next: NextFunctio
 };
 
 /* DELETE users listing. */
-export async function deleteUser (req: Request, res: Response, next: NextFunction) {
-    res.json({
-        msg: "delete user route"
-    });
+export async function deleteUser(req: Request, res: Response, next: NextFunction) {
+    try{
+        const { id } = req.params;
+        const record = await UserInstance.findOne({ where: { id } });
+        if (!record) {
+            return res.status(404).json({
+                msg: "Cannot find user",
+                record
+            });
+        }
+        const deletedRecord = await record.destroy();
+        return res.status(200).json({
+            msg: "User deleted successfully",
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "failed to delete",
+            route: "/delete/:id"
+        })
+    }
 };
 
 
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -19,9 +19,9 @@ router.post('/signup',signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.get("/api", getUsers);
-router.get("/api/id", getSingleUser);
-router.patch("/api/id", updateUser);
-router.delete("/api/id", deleteUser);
+router.get("/api/:id", getSingleUser);
+router.patch("/api/:id", updateUser);
+router.delete("/api/:id", deleteUser);
 
 export default router;
 
